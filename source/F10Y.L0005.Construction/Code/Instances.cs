a5ea32139814a1027d998b995121b51a997ba8f2
davidcoats <david@getsafetycone.com>
2025-04-08 16:45:48 -0700

    Initial commit.

---
+++ source/F10Y.L0005.Construction/Code/Instances.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+namespace F10Y.L0005.Construction
+{
+    public static class Instances
+    {
+		
+    }
+}
---
+++ source/F10Y.L0005/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace F10Y.L0005
+{
+	/// <summary>
+	/// .NET Standard 2.1 platform library.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
